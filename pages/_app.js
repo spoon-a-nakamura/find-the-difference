37fe9d0bcbdd539a66c92ed43ba99d6bc1c1dfad
a-nakamura <a-nakamura@studio-spoon.co.jp>
2021-04-17 21:19:21 +0900

    adobeLoaderをHooksで読み込み

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
+import React, { useEffect } from 'react'
 import '../styles/globals.css'
+import { adobeLoader } from '../fonts/adobeLoader'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if (process.browser) adobeLoader(document)
+  }, [])
   return <Component {...pageProps} />
 }
 
