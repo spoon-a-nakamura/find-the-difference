bee4d08c39cd420fb6b553ba30ebd8395ff47c80
a-nakamura <a-nakamura@studio-spoon.co.jp>
2021-04-19 01:05:05 +0900

    判定の調整

--- pages/animals.js
+++ pages/animals.js
@@ -31,7 +31,6 @@ export default function Animals() {
     </>
   )
 }
-
 const CanvasWrapper = styled.div`
   position: relative;
 `
@@ -45,14 +44,47 @@ const pointStyle = css`
   position: absolute;
   width: 30px;
   height: 30px;
-  border-radius: 30px;
-  border: 5px solid #ead113;
-  transition: all ease 0.3s;
-  opacity: 1;
-  transform: scale(1) translateY(0);
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 100%;
+    border: 5px solid #ead113;
+    transition: all ease-in-out 0.3s;
+    will-change: animation;
+    animation: bound forwards 0.6s ease-in-out;
+  }
   &[data-is-show='false'] {
-    opacity: 0;
-    transform: scale(0.2);
+    &::before {
+      opacity: 0;
+      transform: scale(0.2) translateY(20px);
+      animation: none;
+    }
+  }
+  @keyframes bound {
+    0% {
+      opacity: 0;
+      transform: scale(0.2) translateY(20px);
+    }
+    50% {
+      opacity: 1;
+      transform: scale(1.2) translateY(-10px);
+    }
+    80% {
+      opacity: 1;
+      transform: scale(0.9) translateY(5px);
+    }
+    90% {
+      opacity: 1;
+      transform: scale(1.1) translateY(-5px);
+    }
+    100% {
+      opacity: 1;
+      transform: scale(1) translateY(0px);
+    }
   }
 `
 const CorrectPoint01 = styled.div`
