6ec0c5463d805c3d0842f6431c9d65c5923030fe
a-nakamura <a-nakamura@studio-spoon.co.jp>
2021-04-18 23:21:29 +0900

    ステージ画面の作成

--- pages/animals.js
+++ pages/animals.js
@@ -1,13 +1,33 @@
 import styled from '@emotion/styled'
+import Image from 'next/image'
+import Header from '../components/StageHeader'
+import CountIcons from '../components/CountIcons'
+import CountDownTimer from '../components/CountDownTimer'
 
 export default function Animals() {
   return (
     <>
-      <Title>どうぶつステージ 01</Title>
-      <Descriptions></Descriptions>
+      <Header title='どうぶつステージ  01' name='いぬ' />
+      <CanvasWrapper>
+        <CanvasA>
+          <Image src='/images/animals/01a@3x.png' width='332' height='249' />
+        </CanvasA>
+        <CanvasB>
+          <Image src='/images/animals/01b@3x.png' width='332' height='249' />
+        </CanvasB>
+      </CanvasWrapper>
+      <CountDownTimer />
+      <CountIcons />
     </>
   )
 }
 
-const Title = styled.h1``
-const Descriptions = styled.div``
+const StageWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  height: 100%;
+`
+const CanvasWrapper = styled.div``
+const CanvasA = styled.div``
+const CanvasB = styled.div``
