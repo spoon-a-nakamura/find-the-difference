e3b274bdd00f107fb1b3eb49b5641639754ec9d0
a-nakamura <a-nakamura@studio-spoon.co.jp>
2021-04-18 14:26:53 +0900

    _document.jsをつくったけど、たぶん効いていない……

---
+++ pages/_document.js
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>まちがい探し | スタジオスプーン</title>
+          <link rel='icon' href='/images/common/favicon.ico' />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
