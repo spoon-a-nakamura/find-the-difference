a4fb21665aecd90d1e96aaf39ac4602cbaa9196c
a-nakamura <a-nakamura@studio-spoon.co.jp>
2021-04-18 15:07:59 +0900

    app全体でlayoutコンポーネントを読み込む。headも共通読み込みに。

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,24 @@
+import Head from 'next/head'
 import React, { useEffect } from 'react'
 import { adobeLoader } from '../fonts/adobeLoader'
 import '../styles/globals.scss'
+import Layout from '../components/layout'
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     if (process.browser) adobeLoader(document)
   }, [])
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>まちがい探し | スタジオスプーン</title>
+        <link rel='icon' href='/images/common/favicon.ico' />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  )
 }
 
 export default MyApp
