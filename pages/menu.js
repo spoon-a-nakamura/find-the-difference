fa5ef9248d0615cd286927ad2c71a321b6eaf68f
a-nakamura <a-nakamura@studio-spoon.co.jp>
2021-04-29 17:28:09 +0900

    Menuページの作成

--- pages/menu.js
+++ pages/menu.js
@@ -1,77 +1,212 @@
-import Image from 'next/image'
 import Link from 'next/link'
 import styled from '@emotion/styled'
+import { motion } from 'framer-motion'
 import Container from '../components/Container'
+import { colors } from '../components/Colors'
 
 export default function Menu() {
+  const eyeAnimate = {
+    x: [0, 5, -10, 10, 0],
+    y: [0, 1, 1, -1, 0],
+  }
+  const eyeTransition = {
+    repeat: Infinity,
+    repeatDelay: 1,
+    type: 'tween',
+    duration: 10,
+  }
+  const startAnimate = {
+    scale: [1, 0.95, 1.05, 0.98, 1.01, 0.99, 1],
+  }
+  const startTransition = {
+    repeat: Infinity,
+    repeatDelay: 1,
+    type: 'tween',
+    duration: 2,
+  }
   return (
     <Container>
       <Title>
-        <Image
-          src='/images/common/logo.svg'
-          width={212}
-          height={46.18}
-          alt='スタジオスプーンの間違い探し'
-        />
+        <TitleSub>スタジオスプーンの</TitleSub>
+        <TitleMain>まちがい探し</TitleMain>
       </Title>
+      <Face>
+        <LeftEye animate={eyeAnimate} transition={eyeTransition} />
+        <RightEye animate={eyeAnimate} transition={eyeTransition} />
+      </Face>
+      <Course>
+        <CourseTitleWrapper>
+          <CourseTitle>
+            <CourseTitleOrange>どちらか</CourseTitleOrange>
+            <CourseTitleBlack>の</CourseTitleBlack>
+            <CourseTitleYellow>コース</CourseTitleYellow>
+            <CourseTitleBlack>を</CourseTitleBlack>
+            <CourseTitleGreen>選んでね</CourseTitleGreen>
+          </CourseTitle>
+        </CourseTitleWrapper>
+        <Cards>
+          <Link href='course-animals'>
+            <AnimalCard>
+              <AnimalCardTag />
+              <AnimalCardCaption>ANIMAL</AnimalCardCaption>
+            </AnimalCard>
+          </Link>
 
-      <Description>どちらかのコースを選んでね。</Description>
-      <CardWrapper>
-        <Link href='start-animals'>
-          <Card>
-            <CardTitle>
-              どうぶつ<CardTitleMini>コース</CardTitleMini>
-            </CardTitle>
-          </Card>
-        </Link>
-
-        <Link href='start-magic'>
-          <Card>
-            <CardTitle>
-              まほう<CardTitleMini>コース</CardTitleMini>
-            </CardTitle>
-          </Card>
-        </Link>
-      </CardWrapper>
+          <Link href='course-magic'>
+            <MagicCard>
+              <MagicCardTag />
+              <MagicCardCaption>MAGIC</MagicCardCaption>
+            </MagicCard>
+          </Link>
+        </Cards>
+      </Course>
     </Container>
   )
 }
 
-const Title = styled.h1`
-  margin: 40px 0 70px;
-`
-
-const Description = styled.p`
-  font-size: 16px;
-  margin-bottom: 30px;
-`
-
-const CardWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  flex-wrap: wrap;
-  min-height: 50vh;
-  width: 100%;
-  margin-bottom: auto;
-`
-
-const Card = styled.div`
-  background: rgba(226, 226, 226, 0.3);
-  width: 48%;
-  display: flex;
+const Title = styled.div`
+  color: #fff;
   text-align: center;
-  justify-content: center;
+  background: url(/images/common/heading_bg.svg) center -70px / cover no-repeat;
+  width: 100%;
+  height: 180px;
+  position: absolute;
+  top: 0px;
 `
-
-const CardTitle = styled.h2`
-  font-size: 20px;
+const TitleSub = styled.div`
+  font-size: 16px;
+  margin-top: 17px;
+  margin-bottom: 5px;
+`
+const TitleMain = styled.div`
+  font-size: 28px;
+`
+const Face = styled.div`
+  width: 268px;
+  height: 308px;
+  background: url(/images/common/face@2x.png) center / contain no-repeat;
   display: flex;
-  flex-direction: column;
+  justify-content: center;
+  position: relative;
+  top: 40px;
+  z-index: 2;
+`
+const LeftEye = styled(motion.div)`
+  width: 40px;
+  height: 40px;
+  background: url(/images/common/left_eye@2x.png) center / contain no-repeat;
+  margin-right: 30px;
+  margin-top: 115px;
+  position: relative;
+`
+const RightEye = styled(motion.div)`
+  width: 40px;
+  height: 40px;
+  background: url(/images/common/left_eye@2x.png) center / contain no-repeat;
+  margin-top: 115px;
+  position: relative;
+`
+const Course = styled.div`
+  background: url(/images/common/course_bg.svg) center top / cover no-repeat;
+  width: 100%;
+  min-height: 300px;
+  height: auto;
+  top: -15px;
+  position: relative;
+  z-index: 1;
+`
+const CourseTitleWrapper = styled.div`
+  display: flex;
   justify-content: center;
   align-items: center;
+  margin-top: 30px;
+  &::before {
+    content: '';
+    width: 18px;
+    height: 18px;
+    border-radius: 18px;
+    display: block;
+    background: #eee;
+    margin-right: 10px;
+  }
+  &::after {
+    content: '';
+    width: 18px;
+    height: 18px;
+    border-radius: 18px;
+    display: block;
+    background: #eee;
+    margin-left: 10px;
+  }
 `
-const CardTitleMini = styled.span`
+const CourseTitle = styled.p`
+  font-size: 18px;
+  color: ${colors.black};
+`
+const CourseTitleBlack = styled.span`
+  color: ${colors.black};
+`
+const CourseTitleOrange = styled.span`
+  color: ${colors.orange};
+`
+const CourseTitleYellow = styled.span`
+  color: ${colors.yellow};
+`
+const CourseTitleGreen = styled.span`
+  color: ${colors.green};
+`
+const Cards = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  margin-top: 20px;
+`
+const AnimalCard = styled.div`
+  background: url(/images/common/img_animal@2x.png) center / contain no-repeat;
+  width: 163px;
+  height: 275px;
+  position: relative;
+  display: flex;
+  justify-content: center;
+`
+const AnimalCardTag = styled.div`
+  background: url(/images/common/tag_animal.svg) center / contain no-repeat;
+  width: 156px;
+  height: 61px;
+  position: absolute;
+  left: -20px;
+  top: -5px;
+`
+const AnimalCardCaption = styled.p`
   display: block;
   font-size: 14px;
   margin-top: 10px;
+  position: absolute;
+  bottom: -20px;
+  color: ${colors.orange};
+`
+const MagicCard = styled.div`
+  background: url(/images/common/img_magic@2x.png) center / contain no-repeat;
+  width: 163px;
+  height: 275px;
+  position: relative;
+  display: flex;
+  justify-content: center;
+`
+const MagicCardTag = styled.div`
+  background: url(/images/common/tag_magic.svg) center / contain no-repeat;
+  width: 156px;
+  height: 61px;
+  position: absolute;
+  left: -20px;
+  top: -5px;
+`
+const MagicCardCaption = styled.p`
+  display: block;
+  font-size: 14px;
+  margin-top: 10px;
+  position: absolute;
+  bottom: -20px;
+  color: ${colors.orange};
 `
