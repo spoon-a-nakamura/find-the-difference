70ff9f1507f53875b495a95673129af3fa1c78e5
a-nakamura <a-nakamura@studio-spoon.co.jp>
2021-04-29 20:09:23 +0900

    Containerの整理

--- components/Container.js
+++ components/Container.js
@@ -1,27 +1,45 @@
 import styled from '@emotion/styled'
 import Footer from './Footer'
-import ChangeScreen from './ChangeScreen'
+import { motion } from 'framer-motion'
 import { colors } from '../components/Colors'
 
 export default function Container({ children }) {
+  const animate = {
+    scale: 1,
+    opacity: 1,
+  }
+  const initial = {
+    scale: 0.9,
+    opacity: 0,
+  }
+  const exit = {
+    scale: 0.9,
+    opacity: 0,
+  }
+  const transition = {
+    duration: 0.5,
+  }
   return (
-    <ContainerWrapper>
-      <ChangeScreen>
-        <Main>{children}</Main>
-      </ChangeScreen>
+    <ContainerWrapper
+      animate={animate}
+      initial={initial}
+      exit={exit}
+      transition={transition}
+    >
+      <Main>{children}</Main>
       <Footer />
     </ContainerWrapper>
   )
 }
 
-const ContainerWrapper = styled.div`
+const ContainerWrapper = styled(motion.div)`
   width: 100%;
   height: 100%;
   text-align: center;
   background: ${colors.paleGreen} url(/images/common/bg.svg) center / cover;
 `
 
-const Main = styled.div`
+const Main = styled(motion.div)`
   width: 100%;
   height: 100%;
 `
