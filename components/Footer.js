ffc5055a50794deb8d2f7205430ae25070c25630
a-nakamura <a-nakamura@studio-spoon.co.jp>
2021-04-29 16:25:33 +0900

    Footerの作成

--- components/Footer.js
+++ components/Footer.js
@@ -3,24 +3,28 @@ import styled from '@emotion/styled'
 export default function Footer() {
   return (
     <FooterWrapper>
-      <a
+      <CopyRights
         href='https://studio-spoon.co.jp'
         target='_blank'
         rel='noopener noreferrer'
       >
         &copy; STUDIO SPOON.inc
-      </a>
+      </CopyRights>
     </FooterWrapper>
   )
 }
 
 const FooterWrapper = styled.footer`
   width: 100%;
-  padding: 20px;
-  font-size: 12px;
+  height: 40px;
   background: #f7f7f7;
   display: flex;
   justify-content: center;
   align-items: center;
-  z-index: 10;
+  background: url(/images/common/footer_bg.svg) center 10px / cover repeat-x;
+`
+const CopyRights = styled.a`
+  font-size: 10px;
+  color: #fff;
+  margin-top: 14px;
 `
